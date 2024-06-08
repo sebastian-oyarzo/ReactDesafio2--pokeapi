ea6bf5ef998866c8d4eca9e4f47b1ecd486a554b
sebastian <sebastian.oyarzo.zapata@gmail.com>
2024-06-08 19:58:06 -0400

    terminando.v1

--- src/views/PokemonSeleccionado.jsx
+++ src/views/PokemonSeleccionado.jsx
@@ -8,10 +8,12 @@ export const PokemonSeleccionado = () => {
 
 const [Data, setData] = useState([])
 const [UrlImagen, setUrlImagen] = useState('')
+const [Tipe, setTipe] = useState('')
 
 const PokemonData = async () => {
     const response = await axios.get('https://pokeapi.co/api/v2/pokemon/'+ id)
     setUrlImagen(response.data.sprites.other.dream_world.front_default)
+    setTipe(response.data.types[0].type.name + ' ' + (response.data.types[1] == undefined ? '' : response.data.types[1].type.name)  )
     const dataFiltered = response.data.stats.map((poke) => {
       const arreglo = {
         statsName : poke.stat.name ,
@@ -38,10 +40,13 @@ const PokemonData = async () => {
             {
                 Data.length ?  Data.map((datos , key) => (
                     <p key={key} className="stats">{datos.statsName} : {datos.statsNumber} </p>
+
                 ))
+
                 :
                 <p className="cargando">cargando...</p>
             }
+            <p>tipo : {Tipe} </p>
             </div>
         </div>
     </div>
